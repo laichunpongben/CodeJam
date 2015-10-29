33dbea17e0f1e5d2b45c3e84407d7133c9f046f6
laichunpongben <laichunpongben@gmail.com>
2015-10-29 19:57:45 +0800

    Update and rename Moist.py to moist.py

--- moist.py
+++ moist.py
@@ -3,45 +3,45 @@
 # Practice Round APAC test 2016 
 # Problem C. Moist
 
+# Unsolved
+
 class TestCase:
     def __init__(self):
         self.cards = []
-        
-testCaseFile = open("Moist_C-small-practice-1.in", "r")
-lines = testCaseFile.read().split('\n')
-n = int(lines[0])
-testCases = [0 for x in range(n)]
 
 def initialize_test_cases(lines):
-    global testCases
+    global test_cases
     count = -1
     for index, item in enumerate(lines):
         if index > 0: 
             if item.isdigit(): 
                 count += 1
-                testCases[count] = TestCase()
+                test_cases[count] = TestCase()
             else: 
-                testCases[count].cards.extend([item])
+                test_cases[count].cards.extend([item])
 
-def insertionSortAndCount(mylist):
+def insertion_sort_and_count(mylist):
     count = 0
     for index in range(1, len(mylist)):
-        currentValue = mylist[index]
+        current_value = mylist[index]
         position = index
 
-        if (position > 0 and mylist[position - 1] > currentValue): count += 1
-        while position > 0 and mylist[position - 1] > currentValue:
+        if (position > 0 and mylist[position - 1] > current_value): count += 1
+        while position > 0 and mylist[position - 1] > current_value:
             mylist[position] = mylist[position - 1]
             position = position - 1
     
-        mylist[position] = currentValue
+        mylist[position] = current_value
     return count
 
 def print_all_results():
-    for x in range(len(testCases)):
-        #print(testCases[x].cards)
-        count = insertionSortAndCount(testCases[x].cards)
+    for x in range(len(test_cases)):
+        count = insertion_sort_and_count(test_cases[x].cards)
         print('Case #' + str(x + 1) + ': ' + str(count))
 
+testCaseFile = open("Moist_C-small-practice-1.in", "r")
+lines = testCaseFile.read().split('\n')
+n = int(lines[0])
+test_cases = [0 for x in range(n)]
 initialize_test_cases(lines)
 print_all_results()
