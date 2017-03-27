d69e4a822a02dda22098a4c85d52c03b20c483ac
Lai Chun Pong <laichunpongben@gmail.com>
2017-03-28 00:27:52 +0800

    Remove itertools product

--- 2016/qualification_round/coin_jam.py
+++ 2016/qualification_round/coin_jam.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-import itertools
 
-def get_coins(length):
-    if length >= 2 and isinstance(length, int):
-        permutations = list(itertools.product(['0', '1'], repeat=length-2))
-        return ['1' + ''.join(list(t)) + '1' for t in permutations]
-    else:
-        raise ValueError
+def get_coin(length, i):
+    return '1' + format(i, '#0{0}b'.format(length))[2:] + '1'
 
 def is_coin_jam(coin):
     if len(coin) < 2:
@@ -48,31 +43,65 @@ def is_prime(n):
 
     return True
 
-def get_coin_jams(length, required_count):
-    coins = get_coins(length)
-    coin_jams = []
-    for coin in coins:
-        if is_coin_jam(coin):
-            coin_jams.append(coin)
-        if len(coin_jams) >= required_count:
-            return coin_jams
+def get_divisor(n):
+    """Returns the divisor"""
+    if n == 2:
+        return None
+    if n == 3:
+        return None
+    if n % 2 == 0:
+        return 2
+    if n % 3 == 0:
+        return 3
+
+    i = 5
+    w = 2
+
+    while i * i <= n:
+        if n % i == 0:
+            return i
+
+        i += w
+        w = 6 - w
+
     return None
 
+def get_coin_jams(length, required_count):
+    coin_jams = []
+    i = 0
+    while True:
+        coin = get_coin(length, i)
+        if is_coin_jam(coin):
+            coin_and_disvisors = [coin]
+            for base in range(2,11):
+                base10_int = int(coin, base)
+                divisor = get_divisor(base10_int)
+                coin_and_disvisors.append(divisor)
+            coin_jams.append(coin_and_disvisors)
+        if len(coin_jams) >= required_count:
+            return coin_jams
+        i += 1
+    return None
 
 if __name__ == '__main__':
     print(get_coin_jams(6, 3))
 
-    with open('C-small-practice.in', 'r') as input_file:
-        lines = input_file.readlines()
-    input_count = int(lines[0].replace('\n' ,''))
-    inputs = [tuple([int(_) for _ in line.replace('\n', '').split(' ')]) for line in lines[1:]]
+    data_files = ['C-large-practice']
+    for f in data_files:
+        with open('{0}.in'.format(f), 'r') as input_file:
+            lines = input_file.readlines()
+        input_count = int(lines[0].replace('\n' ,''))
+        inputs = [tuple([int(_) for _ in line.replace('\n', '').split(' ')]) for line in lines[1:]]
 
-    for in_ in inputs:
-        n, j = in_
-        print(get_coin_jams(n, j))
+        i = 1
+        with open('{0}.out'.format(f), 'w') as output_file:
+            for in_ in inputs:
+                n, j = in_
+                coin_jams = get_coin_jams(n, j)
 
-    # i = 1
-    # with open('B-small-practice.out', 'w') as output_file:
-    #     for in_ in inputs:
-    #         output_file.write('Case #{0}: {1}\n'.format(i, get_last_num(in_)))
-    #         i += 1
+                output_file.write('Case #{0}:\n'.format(i))
+                for coin_jam in coin_jams:
+                    output_file.write(' '.join([str(_) for _ in coin_jam]))
+                    output_file.write('\n')
+
+                i += 1
